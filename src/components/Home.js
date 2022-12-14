6f5926dd0b6d09949303a27df5777c0373a7044b
Kunal Mehrotra <kunalmehrotra@Kunals-MacBook-Pro.local>
2022-12-13 23:35:25 -0500

    home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,13 @@
 import { Add, Logout, Search, ShoppingCart } from "@mui/icons-material";
 import Close from "@mui/icons-material/Close";
 import {
-    AppBar,
-    Badge,
-    IconButton,
-    InputBase,
-    Paper,
-    Toolbar,
-    Typography,
+  AppBar,
+  Badge,
+  IconButton,
+  InputBase,
+  Paper,
+  Toolbar,
+  Typography,
 } from "@mui/material";
 import React, { useState, useEffect, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
@@ -19,252 +19,290 @@ import CreateProduct from "./CreateProduct";
 import Orders from "./Cart";
 
 const Home = () => {
-    const [products, setProducts] = useState([]);
-    const [filteredProducts, setFilteredProducts] = useState([]);
-    const [search, setSearch] = useState("");
-    const [error, setError] = useState(false);
-    const [errorMsg, setErrorMsg] = useState("");
-    const [createProduct, setCreateProduct] = useState(false);
-    const [orders, setOrders] = useState(new Set());
-    const [openOrders, setOpenOrders] = useState(false);
-    const [pastOrders, setPastOrders] = useState([]);
+  const [products, setProducts] = useState([]);
+  //   const [productsAPI, setAPIproducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [search, setSearch] = useState("");
+  const [error, setError] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+  const [createProduct, setCreateProduct] = useState(false);
+  const [orders, setOrders] = useState(new Set());
+  const [openOrders, setOpenOrders] = useState(false);
+  const [pastOrders, setPastOrders] = useState([]);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        console.log("URI", process.env.REACT_APP_API_URL);
-        fetch(`${process.env.REACT_APP_API_URL}product`).then((response) => {
-            response.json().then((data) => {
-                setProducts(data.products);
-            });
-        });
-    }, []);
+  useEffect(() => {
+    console.log("URI", process.env.REACT_APP_API_URL);
 
-    useEffect(() => {
-        fetch(
-            `${process.env.REACT_APP_API_URL}user/${window.localStorage.getItem(
-                "user"
-            )}/orders`
-        ).then((res) =>
-            res.json().then((data) => {
-                setPastOrders(data.orders);
-            })
+    const dataSet = async () => {
+      try {
+        const productsBackend = await fetch(
+          `${process.env.REACT_APP_API_URL}product`
         );
-    }, []);
+        const productsAPI = await fetch(`https://fakestoreapi.com/products`);
+        setProducts(productsBackend.data);
 
-    useEffect(() => {
-        setFilteredProducts(products);
-    }, [products]);
+        const mappedAPIData = productsAPI.data.map((data) => ({
+          seller: "test",
+          title: data.heading,
 
-    const handleSearch = (e) => setSearch(e.target.value);
+          source: "thirdparty",
+        }));
 
-    const searchProducts = () => {
-        if (search.length > 0) {
-            setFilteredProducts(
-                products.filter((product) => product.name.includes(search))
-            );
-        } else {
-            setFilteredProducts(products);
-        }
+        console.log(mappedAPIData);
+
+        // productsAPI -> properties diffrent
+        // productsBackend -> different
+        // concatenate both results and setProducts
+        // identifier can be added to differentiate source (source: 'fakestore')
+
+        // {
+        //   ... data
+        //   source: 'fakestore'
+        // }
+      } catch (err) {
+        console.log(err);
+      }
     };
+    dataSet();
+  }, []);
 
-    const logout = () => {
-        window.localStorage.removeItem("user");
-        window.localStorage.removeItem("userType");
-        navigate("/");
-    };
+  //   useEffect(() => {
+  //     console.log("URI", process.env.REACT_APP_API_URL);
+  //     fetch(`https://fakestoreapi.com/products`).then((response) => {
+  //       response.json().then((data) => {
+  //         setAPIproducts(data.products);
+  //       });
+  //     });
+  //   }, []);
 
-    const deleteProduct = async (id) => {
-        const response = await fetch(
-            `${process.env.REACT_APP_API_URL}product/${id}`,
-            {
-                method: "DELETE",
-            }
-        );
+  useEffect(() => {
+    fetch(
+      `${process.env.REACT_APP_API_URL}user/${window.localStorage.getItem(
+        "user"
+      )}/orders`
+    ).then((res) =>
+      res.json().then((data) => {
+        setPastOrders(data.orders);
+      })
+    );
+  }, []);
 
-        const { success } = await response.json();
+  useEffect(() => {
+    setFilteredProducts(products);
+  }, [products]);
 
-        if (success) {
-            setProducts((oldProducts) => oldProducts.filter(({ _id }) => _id !== id));
-        } else {
-            setErrorMsg("Cannot delete product");
-            setError(true);
-        }
-    };
+  const handleSearch = (e) => setSearch(e.target.value);
 
-    const updateProduct = (id, updatedProduct) => {
-        const callback = (oldProducts) => {
-            return oldProducts.map((product) => {
-                if (product._id === id) return { ...product, ...updatedProduct };
-                else return product;
-            });
-        };
-        setProducts(callback);
-    };
+  const searchProducts = () => {
+    if (search.length > 0) {
+      setFilteredProducts(
+        products.filter((product) => product.name.includes(search))
+      );
+    } else {
+      setFilteredProducts(products);
+    }
+  };
 
-    const addNewProduct = (newProduct) => {
-        setProducts((oldProducts) => {
-            oldProducts.push(newProduct);
-            return oldProducts;
-        });
-    };
+  const logout = () => {
+    window.localStorage.removeItem("user");
+    window.localStorage.removeItem("userType");
+    navigate("/");
+  };
 
-    const addToCart = useCallback(
-        (item) => {
-            const updatedOrders = new Set(orders);
-            updatedOrders.add(item);
-            setOrders(updatedOrders);
-        },
-        [orders, setOrders]
+  const deleteProduct = async (id) => {
+    const response = await fetch(
+      `${process.env.REACT_APP_API_URL}product/${id}`,
+      {
+        method: "DELETE",
+      }
     );
 
-    const removeFromCart = useCallback(
-        (item) => {
-            const updatedOrders = new Set(orders);
-            updatedOrders.delete(item);
-            setOrders(updatedOrders);
-        },
-        [orders, setOrders]
+    const { success } = await response.json();
+
+    if (success) {
+      setProducts((oldProducts) => oldProducts.filter(({ _id }) => _id !== id));
+    } else {
+      setErrorMsg("Cannot delete product");
+      setError(true);
+    }
+  };
+
+  const updateProduct = (id, updatedProduct) => {
+    const callback = (oldProducts) => {
+      return oldProducts.map((product) => {
+        if (product._id === id) return { ...product, ...updatedProduct };
+        else return product;
+      });
+    };
+    setProducts(callback);
+  };
+
+  const addNewProduct = (newProduct) => {
+    setProducts((oldProducts) => {
+      oldProducts.push(newProduct);
+      return oldProducts;
+    });
+  };
+
+  const addToCart = useCallback(
+    (item) => {
+      const updatedOrders = new Set(orders);
+      updatedOrders.add(item);
+      setOrders(updatedOrders);
+    },
+    [orders, setOrders]
+  );
+
+  const removeFromCart = useCallback(
+    (item) => {
+      const updatedOrders = new Set(orders);
+      updatedOrders.delete(item);
+      setOrders(updatedOrders);
+    },
+    [orders, setOrders]
+  );
+
+  const orderNow = async () => {
+    let res = await fetch(`${process.env.REACT_APP_API_URL}order/bulk`, {
+      method: "POST",
+      body: JSON.stringify({
+        products: Array.from(orders).map((order) => order._id),
+        email: window.localStorage.getItem("user"),
+        discount: 0,
+      }),
+      headers: {
+        "Content-Type": "Application/json",
+      },
+    });
+    let data = await res.json();
+
+    res = await fetch(
+      `${process.env.REACT_APP_API_URL}user/${window.localStorage.getItem(
+        "user"
+      )}/orders`
     );
 
-    const orderNow = async () => {
-        let res = await fetch(`${process.env.REACT_APP_API_URL}order/bulk`, {
-            method: "POST",
-            body: JSON.stringify({
-                products: Array.from(orders).map((order) => order._id),
-                email: window.localStorage.getItem("user"),
-                discount: 0,
-            }),
-            headers: {
-                "Content-Type": "Application/json",
-            },
-        });
-        let data = await res.json();
+    data = await res.json();
 
-        res = await fetch(
-            `${process.env.REACT_APP_API_URL}user/${window.localStorage.getItem(
-                "user"
-            )}/orders`
-        );
+    setOrders(new Set());
+    setPastOrders(data.orders);
+  };
 
-        data = await res.json();
+  const deleteOrder = async (orderId) => {
+    await fetch(`${process.env.REACT_APP_API_URL}order/${orderId}`, {
+      method: "DELETE",
+    });
 
-        setOrders(new Set());
-        setPastOrders(data.orders);
-    };
+    const res = await fetch(
+      `${process.env.REACT_APP_API_URL}user/${window.localStorage.getItem(
+        "user"
+      )}/orders`
+    );
 
-    const deleteOrder = async (orderId) => {
-        await fetch(`${process.env.REACT_APP_API_URL}order/${orderId}`, {
-            method: "DELETE",
-        });
+    const data = await res.json();
+    setPastOrders(data.orders);
+  };
 
-        const res = await fetch(
-            `${process.env.REACT_APP_API_URL}user/${window.localStorage.getItem(
-                "user"
-            )}/orders`
-        );
-
-        const data = await res.json();
-        setPastOrders(data.orders);
-    };
-
-    return (
-        <div className="home">
-            <AppBar>
-                <Toolbar>
-                    <Typography variant="body1">E-Commerce website</Typography>
-                    <Paper className="search-bar">
-                        <InputBase
-                            sx={{ ml: 1, flex: 1, width: "500px" }}
-                            fullWidth
-                            placeholder="Search products"
-                            onChange={handleSearch}
-                            value={search}
-                            onKeyPress={(e) => {
-                                if (e.code === "Enter") searchProducts();
-                            }}
-                        />
-                        {search.length > 0 ? (
-                            <IconButton
-                                onClick={() => {
-                                    setSearch("");
-                                    setFilteredProducts(products);
-                                }}
-                            >
-                                <Close />
-                            </IconButton>
-                        ) : (
-                            <IconButton onClick={searchProducts}>
-                                <Search />
-                            </IconButton>
-                        )}
-                    </Paper>
-
-                    <div className="right-action-btns">
-                        <IconButton
-                            className="action-btn"
-                            onClick={() => setOpenOrders(true)}
-                        >
-                            <Badge badgeContent={orders.size} color="secondary">
-                                <ShoppingCart />
-                            </Badge>
-                        </IconButton>
-                        {window.localStorage.getItem("userType") === "admin" && (
-                            <IconButton
-                                className="action-btn"
-                                onClick={() => setCreateProduct(true)}
-                            >
-                                <Add />
-                            </IconButton>
-                        )}
-                        <IconButton className="action-btn" onClick={logout}>
-                            <Logout />
-                        </IconButton>
-                    </div>
-                </Toolbar>
-            </AppBar>
-            <div className="catalogue">
-                {filteredProducts.length > 0 ? (
-                    filteredProducts.map((product) => (
-                        <ProductCard
-                            key={product._id}
-                            product={product}
-                            deleteProduct={deleteProduct}
-                            updateProduct={updateProduct}
-                            addToCart={addToCart}
-                        />
-                    ))
-                ) : (
-                    <div>Loading products</div>
-                )}
-            </div>
-            <CreateProduct
-                open={createProduct}
-                handleClose={() => setCreateProduct(false)}
-                allSellers={[].concat.apply([],
-                    products.map((product) => product.sellers)
-                )}
-                addNewProduct={addNewProduct}
+  return (
+    <div className="home">
+      <AppBar>
+        <Toolbar>
+          <Typography variant="body1">E-Commerce website</Typography>
+          <Paper className="search-bar">
+            <InputBase
+              sx={{ ml: 1, flex: 1, width: "500px" }}
+              fullWidth
+              placeholder="Search products"
+              onChange={handleSearch}
+              value={search}
+              onKeyPress={(e) => {
+                if (e.code === "Enter") searchProducts();
+              }}
             />
-            <Orders
-                open={openOrders}
-                orders={orders}
-                handleClose={() => setOpenOrders(false)}
-                removeFromCart={removeFromCart}
-                pastOrders={pastOrders}
-                orderNow={orderNow}
-                deleteOrder={deleteOrder}
-            />
-            <ErrorSnackbar
-                open={error}
-                handleClose={() => {
-                    setError(false);
-                    setErrorMsg("");
+            {search.length > 0 ? (
+              <IconButton
+                onClick={() => {
+                  setSearch("");
+                  setFilteredProducts(products);
                 }}
-                message={errorMsg}
+              >
+                <Close />
+              </IconButton>
+            ) : (
+              <IconButton onClick={searchProducts}>
+                <Search />
+              </IconButton>
+            )}
+          </Paper>
+
+          <div className="right-action-btns">
+            <IconButton
+              className="action-btn"
+              onClick={() => setOpenOrders(true)}
+            >
+              <Badge badgeContent={orders.size} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+            {window.localStorage.getItem("userType") === "admin" && (
+              <IconButton
+                className="action-btn"
+                onClick={() => setCreateProduct(true)}
+              >
+                <Add />
+              </IconButton>
+            )}
+            <IconButton className="action-btn" onClick={logout}>
+              <Logout />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <div className="catalogue">
+        {filteredProducts.length > 0 ? (
+          filteredProducts.map((product) => (
+            <ProductCard
+              key={product._id}
+              product={product}
+              deleteProduct={deleteProduct}
+              updateProduct={updateProduct}
+              addToCart={addToCart}
             />
-        </div>
-    );
+          ))
+        ) : (
+          <div>Loading products</div>
+        )}
+      </div>
+      <CreateProduct
+        open={createProduct}
+        handleClose={() => setCreateProduct(false)}
+        allSellers={[].concat.apply(
+          [],
+          products.map((product) => product.sellers)
+        )}
+        addNewProduct={addNewProduct}
+      />
+      <Orders
+        open={openOrders}
+        orders={orders}
+        handleClose={() => setOpenOrders(false)}
+        removeFromCart={removeFromCart}
+        pastOrders={pastOrders}
+        orderNow={orderNow}
+        deleteOrder={deleteOrder}
+      />
+      <ErrorSnackbar
+        open={error}
+        handleClose={() => {
+          setError(false);
+          setErrorMsg("");
+        }}
+        message={errorMsg}
+      />
+    </div>
+  );
 };
 
 export default Home;
