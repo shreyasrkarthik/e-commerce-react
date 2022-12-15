7d9be5f9254e840664a5007cc1d1c58e018f502a
shreyasrkarthik <shreyas.rkarthik@gmail.com>
2022-12-14 22:53:18 -0500

    Added 3rd user type in login screen and changed password fields.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import {
-    Card,
-    CardContent,
-    CardActions,
-    TextField,
-    FormControl,
-    FormLabel,
-    RadioGroup,
-    FormControlLabel,
-    Radio,
-    Button,
+  Card,
+  CardContent,
+  CardActions,
+  TextField,
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  Button,
 } from "@mui/material";
 import ErrorSnackbar from "./ErrorSnackbar";
 import { useNavigate } from "react-router-dom";
@@ -17,126 +17,133 @@ import { useNavigate } from "react-router-dom";
 import "../css/Login.css";
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [loginFail, setLoginFail] = useState(false);
-    const [userType, setUserType] = useState("shopper");
-    const [errorMsg, setErrorMsg] = useState("");
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginFail, setLoginFail] = useState(false);
+  const [userType, setUserType] = useState("shopper");
+  const [errorMsg, setErrorMsg] = useState("");
+  const navigate = useNavigate();
 
-    const handleLogin = async () => {
-        if (email && password) {
-            console.log("URI", process.env.REACT_APP_API_URL);
-            const response = await fetch(
-                `${process.env.REACT_APP_API_URL}user/login`,
-                {
-                    method: "POST",
-                    body: JSON.stringify({
-                        email,
-                        password,
-                        userType,
-                    }),
-                    headers: {
-                        "Content-Type": "Application/json",
-                    },
-                }
-            );
-            const { success } = await response.json();
-            if (success) {
-                window.localStorage.setItem("user", email);
-                window.localStorage.setItem("userType", userType);
-                navigate("/home");
-            } else {
-                setLoginFail(true);
-                setErrorMsg("Username or password incorrect");
-            }
-        } else {
-            setErrorMsg("Username or password incorrect");
-            setLoginFail(true);
+  const handleLogin = async () => {
+    if (email && password) {
+      console.log("URI", process.env.REACT_APP_API_URL);
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}user/login`,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email,
+            password,
+            userType,
+          }),
+          headers: {
+            "Content-Type": "Application/json",
+          },
         }
-    };
+      );
+      const { success } = await response.json();
+      if (success) {
+        window.localStorage.setItem("user", email);
+        window.localStorage.setItem("userType", userType);
+        navigate("/home");
+      } else {
+        setLoginFail(true);
+        setErrorMsg("Username or password incorrect");
+      }
+    } else {
+      setErrorMsg("Username or password incorrect");
+      setLoginFail(true);
+    }
+  };
 
-    const handleRegistration = async () => {
-        navigate("/register");
-    };
+  const handleRegistration = async () => {
+    navigate("/register");
+  };
 
-    const handleEmailInput = (e) => setEmail(e.target.value);
-    const handlePassInput = (e) => setPassword(e.target.value);
-    const handleRadio = (e) => setUserType(e.target.value);
+  const handleEmailInput = (e) => setEmail(e.target.value);
+  const handlePassInput = (e) => setPassword(e.target.value);
+  const handleRadio = (e) => setUserType(e.target.value);
 
-    return (
-        <div className="login">
-            <Card className="login-card">
-                <div className="login-header">Login</div>
-                <CardContent className="input-flex">
-                    <div className="input-group">
-                        <div className="login-input">
-                            <TextField
-                                variant="standard"
-                                label="Email"
-                                onChange={handleEmailInput}
-                            />
-                        </div>
-                        <div className="login-input">
-                            <TextField
-                                variant="standard"
-                                label="Password"
-                                onChange={handlePassInput}
-                            />
-                        </div>
-                    </div>
-                    <div className="radio-group">
-                        <FormControl component="fieldset">
-                            <FormLabel component="legend">Role</FormLabel>
-                            <RadioGroup
-                                aria-label="role"
-                                defaultValue="shopper"
-                                name="role-radio-buttons"
-                                onChange={handleRadio}
-                            >
-                                <FormControlLabel
-                                    value="shopper"
-                                    control={<Radio />}
-                                    label="Shopper"
-                                />
-                                <FormControlLabel
-                                    value="admin"
-                                    control={<Radio />}
-                                    label="Admin"
-                                />
-                            </RadioGroup>
-                        </FormControl>
-                    </div>
-                </CardContent>
-                <CardActions>
-                    <Button
-                        className="login-btn"
-                        variant="contained"
-                        onClick={handleLogin}
-                    >
-                        Login
-                    </Button>
-                </CardActions>
-                <CardActions>
-                    <Button
-                        className="login-btn"
-                        variant="contained"
-                        onClick={handleRegistration}
-                    >
-                        Sign Up
-                    </Button>
-                </CardActions>
-            </Card>
-            <ErrorSnackbar
-                open={loginFail}
-                handleClose={() => {
-                    setLoginFail(false);
-                    setErrorMsg("");
-                }}
-                message={errorMsg}
-            />
-        </div>
-    );
+  return (
+    <div className="login">
+      <Card className="login-card">
+        <div className="login-header">Login</div>
+        <CardContent className="input-flex">
+          <div className="input-group">
+            <div className="login-input">
+              <TextField
+                variant="standard"
+                label="Email"
+                onChange={handleEmailInput}
+              />
+            </div>
+            <div className="login-input">
+              <TextField
+                variant="standard"
+                type="password"
+                label="Password"
+                onChange={handlePassInput}
+              />
+            </div>
+          </div>
+          <div className="radio-group">
+            <FormControl component="fieldset">
+              <FormLabel component="legend">Role</FormLabel>
+              <RadioGroup
+                aria-label="role"
+                defaultValue="shopper"
+                name="role-radio-buttons"
+                onChange={handleRadio}
+              >
+                <FormControlLabel
+                  value="shopper"
+                  control={<Radio />}
+                  label="Shopper"
+                />
+                <FormControlLabel
+                  value="admin"
+                  control={<Radio />}
+                  label="Admin"
+                />
+
+                <FormControlLabel
+                  value="seller"
+                  control={<Radio />}
+                  label="Seller"
+                />
+              </RadioGroup>
+            </FormControl>
+          </div>
+        </CardContent>
+        <CardActions>
+          <Button
+            className="login-btn"
+            variant="contained"
+            onClick={handleLogin}
+          >
+            Login
+          </Button>
+        </CardActions>
+        <CardActions>
+          <Button
+            className="login-btn"
+            variant="contained"
+            onClick={handleRegistration}
+          >
+            Sign Up
+          </Button>
+        </CardActions>
+      </Card>
+      <ErrorSnackbar
+        open={loginFail}
+        handleClose={() => {
+          setLoginFail(false);
+          setErrorMsg("");
+        }}
+        message={errorMsg}
+      />
+    </div>
+  );
 };
 
 export default Login;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import {
-    Card,
-    CardContent,
-    CardActions,
-    TextField,
-    FormControl,
-    FormLabel,
-    RadioGroup,
-    FormControlLabel,
-    Radio,
-    Button,
+  Card,
+  CardContent,
+  CardActions,
+  TextField,
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  Button,
 } from "@mui/material";
 import ErrorSnackbar from "./ErrorSnackbar";
 import { useNavigate } from "react-router-dom";
@@ -17,145 +17,149 @@ import { useNavigate } from "react-router-dom";
 import "../css/Login.css";
 
 const Register = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [phone, setPhone] = useState("");
-    const [loginFail, setLoginFail] = useState(false);
-    const [userType, setUserType] = useState("SHOPPER");
-    const [errorMsg, setErrorMsg] = useState("");
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [loginFail, setLoginFail] = useState(false);
+  const [userType, setUserType] = useState("SHOPPER");
+  const [errorMsg, setErrorMsg] = useState("");
+  const navigate = useNavigate();
 
-    const handleRegister = async () => {
-        if (email && password && firstName && lastName && phone) {
-            console.log("URI", process.env.REACT_APP_API_URL);
-            console.log()
-            const response = await fetch(
-                `${process.env.REACT_APP_API_URL}user/create`,
-                {
-                    method: "POST",
-                    body: JSON.stringify({
-                        "firstName" : firstName,
-                        "lastName" : lastName,
-                        "email" : email,
-                        "phone" : phone,
-                        "password" : password,
-                        "userType" : userType,
-                    }),
-                    headers: {
-                        "Content-Type": "Application/json",
-                    },
-                }
-            );
-            const success = await response.json();
-            if (success!=null) {
-                window.localStorage.setItem("user", email);
-                window.localStorage.setItem("userType", userType);
-                console.log(success)
-                navigate("/home");
-            } else {
-                setLoginFail(true);
-                setErrorMsg("Registration failed. Try again!");
-            }
-        } else {
-            setErrorMsg("Valid email and password are required");
-            setLoginFail(true);
+  const handleRegister = async () => {
+    if (email && password && firstName && lastName && phone) {
+      console.log("URI", process.env.REACT_APP_API_URL);
+      console.log();
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}user/create`,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            phone: phone,
+            password: password,
+            userType: userType,
+          }),
+          headers: {
+            "Content-Type": "Application/json",
+          },
         }
-    };
+      );
+      const success = await response.json();
+      if (success != null) {
+        window.localStorage.setItem("user", email);
+        window.localStorage.setItem("userType", userType);
+        console.log(success);
+        navigate("/home");
+      } else {
+        setLoginFail(true);
+        setErrorMsg("Registration failed. Try again!");
+      }
+    } else {
+      setErrorMsg("Enter valid input in all the fields.");
+      setLoginFail(true);
+    }
+  };
 
-    const handleEmailInput = (e) => {
-        if(/\S+@\S+\.\S+/.test(e.target.value)){
-            setEmail(e.target.value)
-        }
-    };
-    const handlePassInput = (e) => setPassword(e.target.value);
-    const handleFirstNameInput = (e) => setFirstName(e.target.value);
-    const handleLastNameInput = (e) => setLastName(e.target.value);
-    const handlePhoneInput = (e) => setPhone(e.target.value);
-    const handleRadio = (e) => setUserType(e.target.value);
+  const handleEmailInput = (e) => {
+    if (/\S+@\S+\.\S+/.test(e.target.value)) {
+      setEmail(e.target.value);
+    }
+  };
+  const handlePassInput = (e) => setPassword(e.target.value);
+  const handleFirstNameInput = (e) => setFirstName(e.target.value);
+  const handleLastNameInput = (e) => setLastName(e.target.value);
+  const handlePhoneInput = (e) => setPhone(e.target.value);
+  const handleRadio = (e) => setUserType(e.target.value);
 
-
-    return (
-        <div className="login">
-            <Card className="login-card">
-                <div className="login-header">Sign Up</div>
-                <CardContent className="input-flex">
-                    <div className="input-group">
-                        <div className="login-input">
-                            <TextField
-                                variant="standard"
-                                label="First Name"
-                                onChange={handleFirstNameInput}
-                            />
-                        </div>
-                        <div className="login-input">
-                            <TextField
-                                variant="standard"
-                                label="Last Name"
-                                onChange={handleLastNameInput}
-                            />
-                        </div>
-                        <div className="login-input">
-                            <TextField
-                                variant="standard"
-                                label="Email"
-                                onChange={handleEmailInput}
-                            />
-                        </div>
-                        <div className="login-input">
-                            <TextField
-                                variant="standard"
-                                label="Password"
-                                onChange={handlePassInput}
-                            />
-                        </div>
-                        <div className="login-input">
-                            <TextField
-                                variant="standard"
-                                label="Phone"
-                                onChange={handlePhoneInput}
-                            />
-                        </div>
-                    </div>
-                    <div className="radio-group">
-                        <FormControl component="fieldset">
-                            <FormLabel component="legend">Role</FormLabel>
-                            <RadioGroup
-                                aria-label="role"
-                                defaultValue="SHOPPER"
-                                name="role-radio-buttons"
-                                onChange={handleRadio}
-                            >
-                                <FormControlLabel
-                                    value="SHOPPER"
-                                    control={<Radio />}
-                                    label="Shopper"
-                                />
-                            </RadioGroup>
-                        </FormControl>
-                    </div>
-                </CardContent>
-                <CardActions>
-                    <Button
-                        className="login-btn"
-                        variant="contained"
-                        onClick={handleRegister}
-                    >
-                        Submit
-                    </Button>
-                </CardActions>
-            </Card>
-            <ErrorSnackbar
-                open={loginFail}
-                handleClose={() => {
-                    setLoginFail(false);
-                    setErrorMsg("");
+  return (
+    <div className="login">
+      <Card className="login-card">
+        <div className="login-header">Sign Up</div>
+        <CardContent className="input-flex">
+          <div className="input-group">
+            <div className="login-input">
+              <TextField
+                variant="standard"
+                label="First Name"
+                onChange={handleFirstNameInput}
+              />
+            </div>
+            <div className="login-input">
+              <TextField
+                variant="standard"
+                label="Last Name"
+                onChange={handleLastNameInput}
+              />
+            </div>
+            <div className="login-input">
+              <TextField
+                variant="standard"
+                label="Email"
+                onChange={handleEmailInput}
+              />
+            </div>
+            <div className="login-input">
+              <TextField
+                variant="standard"
+                type="password"
+                label="Password"
+                onChange={handlePassInput}
+              />
+            </div>
+            <div className="login-input">
+              <TextField
+                variant="standard"
+                label="Phone"
+                type="number"
+                inputProps={{
+                  pattern: "[0-9]{10}",
                 }}
-                message={errorMsg}
-            />
-        </div>
-    );
+                onChange={handlePhoneInput}
+              />
+            </div>
+          </div>
+          <div className="radio-group">
+            <FormControl component="fieldset">
+              <FormLabel component="legend">Role</FormLabel>
+              <RadioGroup
+                aria-label="role"
+                defaultValue="SHOPPER"
+                name="role-radio-buttons"
+                onChange={handleRadio}
+              >
+                <FormControlLabel
+                  value="SHOPPER"
+                  control={<Radio />}
+                  label="Shopper"
+                />
+              </RadioGroup>
+            </FormControl>
+          </div>
+        </CardContent>
+        <CardActions>
+          <Button
+            className="login-btn"
+            variant="contained"
+            onClick={handleRegister}
+          >
+            Submit
+          </Button>
+        </CardActions>
+      </Card>
+      <ErrorSnackbar
+        open={loginFail}
+        handleClose={() => {
+          setLoginFail(false);
+          setErrorMsg("");
+        }}
+        message={errorMsg}
+      />
+    </div>
+  );
 };
 
 export default Register;
